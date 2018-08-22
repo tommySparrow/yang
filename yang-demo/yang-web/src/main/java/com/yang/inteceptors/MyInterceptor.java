371ac3f351230abd6c4667ece3d69d06464aa5b3
jm90_yang <jm90_yang@163.com>
2018-08-22 22:49:44 +0800

    00

---
+++ yang-demo/yang-web/src/main/java/com/yang/inteceptors/MyInterceptor.java
@@ -0,0 +1,22 @@
+package com.yang.inteceptors;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MyInterceptor implements HandlerInterceptor {
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+        System.out.println("拦截器拦截请求");
+        return true;
+    }
+
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+
+    }
+}
